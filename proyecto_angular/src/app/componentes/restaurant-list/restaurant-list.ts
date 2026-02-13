f7d1843b3f10f51d8a49f9800e46b5ddc6d04c91
lucia <luciaandreu@gmail.com>
2026-02-13 16:50:52 +0100

    intentar arreglar

--- proyecto_angular/src/app/componentes/restaurant-list/restaurant-list.ts
+++ proyecto_angular/src/app/componentes/restaurant-list/restaurant-list.ts
@@ -3,6 +3,7 @@ import { RouterLink } from '@angular/router';
 import { RestaurantItem } from '../restaurant-item/restaurant-item';
 import { IPlato } from '../../interfaces/i-plato';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-restaurant-list',
@@ -12,25 +13,38 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RestaurantList {
 
+  private platosSubject = new BehaviorSubject<IPlato[]>([]);
+  platos$ = this.platosSubject.asObservable();
+  
   platos: IPlato[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarPlatos();
+
+    // Suscribirse para llenar el array normal que usa el HTML con @for
+    this.platos$.subscribe(data => {
+      this.platos = data;
+    });
   }
+
+  
 
   cargarPlatos() {
     this.http.get<IPlato[]>('http://localhost:3000/dishes')
       .subscribe(data => {
         console.log(data); 
         // solo visibles en el menú
-        this.platos = data.filter(p => p.enabled);
+        const visibles = data.filter(p => p.enabled);
+        this.platosSubject.next(visibles); // actualiza el BehaviorSubject
       });
   }
   orderPriceLessMore() { // de mayor a menor
-  this.platos = [...this.platos].sort((a, b) => a.price - b.price);
+  const current = this.platosSubject.getValue();
+  this.platosSubject.next([...current].sort((a, b) => a.price - b.price));
 }
 
 orderPriceMoreLess() { //de menor a mayor
-  this.platos = [...this.platos].sort((a, b) => b.price - a.price);
+  const current = this.platosSubject.getValue();
+  this.platosSubject.next([...current].sort((a, b) => b.price - a.price));
 }
 }
